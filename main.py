e53e701916ff004e99815c9c8c5449e619d88a08
Marco Pulido <marcopulido00@gmail.com>
2019-05-04 20:16:14 +0800

    Added debouncer.py and changed main.py so that it uses the debouncer module. Also made test folder for testing modular programming in python and  added init files to pydnet and test folders.

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import RPi.GPIO as GPIO
 import time
 import sys
+from debouncer import *
+#from test1 import *
+#from test.test2 import *
 #import caption.py
 #import webcam.py
 import threading
 
-sys.path.append('/home/pi/Desktop/ETA/pydnet-master/pydnet-master/') #add pydnet folder to sys.path so that we can import pydnet files directly
+sys.path.append('/home/pi/Desktop/ETA/pydnet-master/pydnet-master/') 
+#add pydnet folder to sys.path so that we can import pydnet files directly
+sys.path.append('/home/pi/Desktop/ETA/test/')
+#test path
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
+
+#test1()
+#test2()
 
 #Important pins (Broadcomm notation)
 trigger_get_caption = 15
@@ -34,12 +44,32 @@ def restart(pin):
 
 GPIO.setup([trigger_get_caption, trigger_toggle_depth, trigger_volume_up, trigger_volume_down, trigger_shutdown, trigger_restart], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-GPIO.add_event_detect(trigger_get_caption, GPIO.FALLING, get_image_caption)
-GPIO.add_event_detect(trigger_toggle_depth, GPIO.FALLING, toggle_depth)
-GPIO.add_event_detect(trigger_volume_up, GPIO.FALLING, volume_up)
-GPIO.add_event_detect(trigger_volume_down, GPIO.FALLING, volume_down)
-GPIO.add_event_detect(trigger_shutdown, GPIO.FALLING, shutdown)
-GPIO.add_event_detect(trigger_restart, GPIO.FALLING, restart)
+cb1 = ButtonHandler(trigger_get_caption, get_image_caption, edge='falling', bouncetime=100)
+cb1.start()
+
+cb2 = ButtonHandler(trigger_toggle_depth, toggle_depth, edge='falling', bouncetime=100)
+cb2.start()
+
+cb3 = ButtonHandler(trigger_volume_up, volume_up, edge='falling', bouncetime=100)
+cb3.start()
+
+cb4 = ButtonHandler(trigger_volume_down, volume_down, edge='falling', bouncetime=100)
+cb4.start()
+
+cb5 = ButtonHandler(trigger_shutdown, shutdown, edge='falling', bouncetime=100)
+cb5.start()
+
+cb6 = ButtonHandler(trigger_restart, restart, edge='falling', bouncetime=100)
+cb6.start()
+
+GPIO.add_event_detect(trigger_get_caption, GPIO.FALLING, callback=cb1)
+GPIO.add_event_detect(trigger_toggle_depth, GPIO.FALLING, callback=cb2)
+GPIO.add_event_detect(trigger_volume_up, GPIO.FALLING, callback=cb3)
+GPIO.add_event_detect(trigger_volume_down, GPIO.FALLING, callback=cb4)
+GPIO.add_event_detect(trigger_shutdown, GPIO.FALLING, callback=cb5)
+GPIO.add_event_detect(trigger_restart, GPIO.FALLING, callback=cb6)
+
+
 
 while True:
     time.sleep(1e6)
