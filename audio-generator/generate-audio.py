377c1298b93e01b80b1d4004c66d498c38db4536
Marco Pulido <marcopulido00@gmail.com>
2019-05-08 01:32:06 +0800

    test
    testing - see process documentation

---
+++ audio-generator/generate-audio.py
@@ -0,0 +1,10 @@
+import matplotlib.pyplot as pyplot
+import numpy as numpy
+
+x = numpy.linspace(0, 2 * numpy.pi, 1000)
+y = numpy.sin(x)
+pyplot.plot(x, numpy.sin(x))
+pyplot.show()
+
+
+print("Asd")
