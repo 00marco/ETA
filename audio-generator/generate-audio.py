a406f33449a57f4ae41a33f0c7246152444f688a
Marco Pulido <marcopulido00@gmail.com>
2020-02-01 11:01:24 +0800

    First working version

--- audio-generator/generate-audio.py
+++ audio-generator/generate-audio.py
@@ -1,9 +1,35 @@
+from struct import pack
+from math import sin, pi
+import wave
+import random
+import pygame
 
-import matplotlib.pyplot as pyplot
-import numpy as numpy
 
-x = numpy.linspace(0, 2 * numpy.pi, 1000)
-y = numpy.sin(x)
-pyplot.plot(x, numpy.sin(x))
-pyplot.show()
+# RATE = 44100
+# freq = 2000
 
+
+# wv = wave.open(str(freq) + "Hz.wav", 'w')
+# wv.setparams((2,2,RATE,0,'NONE','not compressed'))
+# maxVol = 2**15-1.0
+# stereoMultiplier = 1.0
+# wvData = bytes("", 'utf-8')
+# for i in range(0, RATE*3):
+#     wvData += pack('f',maxVol*sin(i*freq/RATE)*(1-stereoMultiplier))
+#     wvData += pack('f',maxVol*sin(i*freq/RATE)*(stereoMultiplier))
+
+# wv.writeframes(wvData)
+# wv.close()
+
+pygame.mixer.init(frequency=44000, size=-16, channels=2, buffer=4096)
+m = pygame.mixer.Sound("../../audio-files/Audio-files-for-ETA/audiocheck.net_sin_200Hz_-3dBFS_0.25s.wav")
+n = pygame.mixer.Sound("../../audio-files/Audio-files-for-ETA/audiocheck.net_sin_200Hz_-3dBFS_0.25s.wav")
+
+# pygame.mixer.Channel(1).play(m,-1)
+# pygame.mixer.Channel(2).play(n,-1)
+
+channel0 = m.play()
+channel0.set_volume(1.0, 0.0)
+
+channel1 = n.play()
+channel1.set_volume(0.0, 1.0)
--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import RPi.GPIO as GPIO
+from gpiozero import Button
 import time
 import sys
 from debouncer import *
 #from test1 import *
 #from test.test2 import *
-import caption
+#import caption
 #import webcam.py
 import threading
 
@@ -20,14 +20,20 @@ GPIO.setwarnings(False)
 #test2i()
 
 #Important pins (Broadcomm notation)
-trigger_get_caption = 15
-trigger_toggle_depth = 4
+trigger_get_caption = 17
+trigger_toggle_depth = 27
 
-trigger_volume_up = 14
-trigger_volume_down = 27
+trigger_volume_up = 15
+trigger_volume_down = 18
 
-trigger_shutdown = 18
+trigger_shutdown = 22
 trigger_restart = 17
+
+button_get_caption = Button(trigger_get_caption)
+button_toggle_depth = Button(trigger_toggle_depth)
+button_volume_up = Button(trigger_volume_up)
+button_volume_down = Button(trigger_volume_down)
+button_shutdown = Button(trigger_shutdown)
 
 def get_caption(pin):
     start = time.time()
@@ -63,32 +69,43 @@ def restart(pin):
 
 
 
-GPIO.setup([trigger_get_caption, trigger_toggle_depth, trigger_volume_up, trigger_volume_down, trigger_shutdown, trigger_restart], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-cb1 = ButtonHandler(trigger_get_caption, get_caption, edge='falling', bouncetime=100)
-cb1.start()
+button_get_caption.when_pressed = get_caption
+button_toggle_depth.when_pressed  = toggle_depth
+button_volume_up.when_pressed  = volume_up
+button_volume_down.when_pressed  = volume_down
+button_shutdown.when_pressed  = shutdown
 
-cb2 = ButtonHandler(trigger_toggle_depth, toggle_depth, edge='falling', bouncetime=100)
-cb2.start()
+button_get_caption.wait_for_press()
+button_toggle_depth.wait_for_press()
+button_volume_up.wait_for_press()
+button_volume_down.wait_for_press()
+button_shutdown.wait_for_press()
+#GPIO.setup([trigger_get_caption, trigger_toggle_depth, trigger_volume_up, trigger_volume_down, trigger_shutdown, trigger_restart], GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
-cb3 = ButtonHandler(trigger_volume_up, volume_up, edge='falling', bouncetime=100)
-cb3.start()
+#cb1 = ButtonHandler(trigger_get_caption, get_caption, edge='falling', bouncetime=100)
+#cb1.start()
 
-cb4 = ButtonHandler(trigger_volume_down, volume_down, edge='falling', bouncetime=100)
-cb4.start()
+#cb2 = ButtonHandler(trigger_toggle_depth, toggle_depth, edge='falling', bouncetime=100)
+#cb2.start()
 
-cb5 = ButtonHandler(trigger_shutdown, shutdown, edge='falling', bouncetime=100)
-cb5.start()
+#cb3 = ButtonHandler(trigger_volume_up, volume_up, edge='falling', bouncetime=100)
+#cb3.start()
 
-cb6 = ButtonHandler(trigger_restart, restart, edge='falling', bouncetime=100)
-cb6.start()
+#cb4 = ButtonHandler(trigger_volume_down, volume_down, edge='falling', bouncetime=100)
+#cb4.start()
 
-GPIO.add_event_detect(trigger_get_caption, GPIO.FALLING, get_caption)
-GPIO.add_event_detect(trigger_toggle_depth, GPIO.FALLING, toggle_depth)
-GPIO.add_event_detect(trigger_volume_up, GPIO.FALLING, volume_up)
-GPIO.add_event_detect(trigger_volume_down, GPIO.FALLING, callback=cb4)
-GPIO.add_event_detect(trigger_shutdown, GPIO.FALLING, callback=cb5)
-GPIO.add_event_detect(trigger_restart, GPIO.FALLING, callback=cb6)
+#cb5 = ButtonHandler(trigger_shutdown, shutdown, edge='falling', bouncetime=100)
+#cb5.start()
+
+#cb6 = ButtonHandler(trigger_restart, restart, edge='falling', bouncetime=100)
+#cb6.start()
+
+#GPIO.add_event_detect(trigger_get_caption, GPIO.FALLING, callback=get_caption)
+#GPIO.add_event_detect(trigger_toggle_depth, GPIO.FALLING, callback=toggle_depth)
+#GPIO.add_event_detect(trigger_volume_up, GPIO.FALLING, callback=volume_up)
+#GPIO.add_event_detect(trigger_volume_down, GPIO.FALLING, callback=volume_down)
+#GPIO.add_event_detect(trigger_shutdown, GPIO.FALLING, callback=shutdown)
 
 while True:
     time.sleep(1e6)
